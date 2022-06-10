74101ae4d22a8d93bcaa4c279241d7bb5e2a3539
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-10 17:49:33 +0800

    Created Icon button global component

--- screens/event/Events.js
+++ screens/event/Events.js
@@ -1,11 +1,19 @@
 import { View, Text, StyleSheet, Button } from "react-native";
+import IconButton from "../../components/IconButton";
+import color from "../../constants/color";
 
 const Events = ({ navigation }) => {
   return (
     <View>
       <Text>This is an Events screen</Text>
-      <Button
+      {/* <Button
         title="Go to rewards"
+        onPress={() => navigation.navigate("Rewards")}
+      /> */}
+      <IconButton
+        name="search-outline"
+        size={30}
+        color={color.Grey500}
         onPress={() => navigation.navigate("Rewards")}
       />
     </View>
