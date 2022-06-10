55519b669ee53bcc7d868b7f150f750dfc18fc41
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-10 17:18:54 +0800

    Added global color constant and fonts

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
 import RootStackNavigator from "./navigation/RootStackNavigator";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    SourceSansRegular: require("./assets/fonts/SourceSansPro-Regular.ttf"),
+    SourceSansSemiBold: require("./assets/fonts/SourceSansPro-SemiBold.ttf"),
+    SourceSansBold: require("./assets/fonts/SourceSansPro-Bold.ttf"),
+    SourceSansBlack: require("./assets/fonts/SourceSansPro-Black.ttf"),
+    SourceSansLight: require("./assets/fonts/SourceSansPro-Light.ttf"),
+  });
+
+  if (!loaded) {
+    //load fonts
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="auto" />
---
+++ constants/color.js
@@ -0,0 +1,17 @@
+const color = {
+  Emerald: "#38595E",
+  OffWhite: "#FFFBF8",
+  Grey100: "#E5E3E4",
+  Grey200: "#F5F5F5",
+  Grey300: "#DCDCDC",
+  Grey400: "#929292",
+  Grey500: "#6D6D6D",
+  Grey600: "#222222",
+  Blue: "#C3E1EB",
+  Yellow: "#F8DBA3",
+  Pink: "#F3D0D4",
+  Black: "#000000",
+  White: "#FFFFFF",
+};
+
+export default color;
