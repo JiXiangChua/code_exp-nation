745c980b389c89e01bb927e6556567f1a4a512fc
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-11 12:12:43 +0800

    Rename font loaded state

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import RootStackNavigator from "./navigation/RootStackNavigator";
 import { useFonts } from "expo-font";
 
 export default function App() {
-  const [loaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     SourceSansRegular: require("./assets/fonts/SourceSansPro-Regular.ttf"),
     SourceSansSemiBold: require("./assets/fonts/SourceSansPro-SemiBold.ttf"),
     SourceSansBold: require("./assets/fonts/SourceSansPro-Bold.ttf"),
@@ -11,8 +11,8 @@ export default function App() {
     SourceSansLight: require("./assets/fonts/SourceSansPro-Light.ttf"),
   });
 
-  if (!loaded) {
-    //load fonts
+  if (!fontsLoaded) {
+    //if fonts not loaded, we return nothing
     return null;
   }
 
