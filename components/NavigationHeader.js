5ce06de07ea9e890d7c7d5c1c438da13a97ae543
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-11 17:57:22 +0800

    Created navigation header global component

---
+++ components/NavigationHeader.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const NavigationHeader = (props) => {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={[globalStyle.header2, { color: color.Emerald }]}>
+        {props.title}
+      </Text>
+      {props.iconName && (
+        <IconButton
+          name={props.iconName}
+          size={props.iconSize}
+          color={props.iconColor}
+        />
+      )}
+      {props.imageName && (
+        <Image
+          source={props.image}
+          style={{ width: props.iconSize, height: props.iconSize }}
+        />
+      )}
+    </View>
+  );
+};
+
+export default NavigationHeader;
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+});
