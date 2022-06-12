09bfff67a19e32173b4e2d6f854d89530c36b425
Desmond <desmondtandx@gmail.com>
2022-06-12 22:40:48 +0800

    Updated looks of announcements inside MailCard

--- screens/inbox/components/MailCard.js
+++ screens/inbox/components/MailCard.js
@@ -5,8 +5,22 @@ import color from "../../../constants/color";
 const MailCard = (props) => {
   return (
     <View style={styles.announcementContainer}>
-      <Text style={globalStyle.body1Bold}>{props.sender}</Text>
-      <Text style={globalStyle.body2}>{props.caption}</Text>
+      <View style={styles.firstRow}>
+        <Text style={[globalStyle.body1Bold, styles.senderContainer]}>
+          {props.sender}
+        </Text>
+        <Text
+          style={[globalStyle.body2, styles.dateContainer]}
+          color={color.Grey500}
+        >
+          {props.date}
+        </Text>
+      </View>
+      <View style={styles.secondRow}>
+        <Text style={[globalStyle.body2, styles.captionContainer]}>
+          {props.caption}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -15,13 +29,33 @@ export default MailCard;
 
 const styles = StyleSheet.create({
   announcementContainer: {
-    padding: 20,
-    margin: 10,
+    margin: 5,
     borderWidth: 1,
     borderRadius: 10,
-    shadowColor: "red",
-    shadowRadius: 100,
-    shadowOpacity: 0.2,
-    shadowOffset: { x: 0, height: 3 },
+  },
+  firstRow: {
+    flexDirection: "row",
+    padding: 15,
+  },
+  secondRow: {
+    flexDirection: "row",
+    padding: 15,
+    marginTop: 10,
+    marginBottom: 40,
+  },
+  senderContainer: {
+    position: "absolute",
+    left: 10,
+    top: 10,
+  },
+  dateContainer: {
+    position: "absolute",
+    right: 15,
+    top: 10,
+  },
+  captionContainer: {
+    position: "absolute",
+    left: 15,
+    top: 10,
   },
 });
