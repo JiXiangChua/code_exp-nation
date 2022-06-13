7c58176ad2ca2795e1529935ff1834ca73749431
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-13 23:36:53 +0800

    Edited the UI for events card and added points to grab for each event

--- screens/event/components/EventsCard.js
+++ screens/event/components/EventsCard.js
@@ -3,15 +3,28 @@ import color from "../../../constants/color";
 import globalStyle from "../../../constants/globalStyle";
 
 const EventsCard = ({ item, onPress }) => {
+  console.log(item);
   return (
     <TouchableOpacity
       onPress={onPress}
       style={[styles.item, globalStyle.dropShadow]}
     >
-      <Image style={{ width: "100%" }} source={item.imageBackground} />
+      <View>
+        <Image
+          style={{ width: "100%", zIndex: 0 }}
+          source={item.imageBackground}
+        />
+        <View style={styles.pointsContainer}>
+          <Text style={[globalStyle.body1Bold, { color: color.White }]}>
+            {item.pointsAvailable} points
+          </Text>
+        </View>
+      </View>
       <View style={styles.cardtextContainer}>
         <Text style={[globalStyle.header2]}>{item.title}</Text>
-        <Text style={[globalStyle.body1]}>{item.content}</Text>
+        <Text style={[globalStyle.body1, { letterSpacing: 1 }]}>
+          {item.content}
+        </Text>
       </View>
     </TouchableOpacity>
   );
@@ -22,7 +35,7 @@ export default EventsCard;
 const styles = StyleSheet.create({
   cardtextContainer: {
     marginLeft: 20,
-    color: color.Black,
+    color: color.White,
   },
   item: {
     backgroundColor: color.White,
@@ -30,7 +43,17 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     marginHorizontal: 21.5,
     borderRadius: 10,
-    borderColor: color.Black,
-    borderWidth: 1,
+  },
+  pointsContainer: {
+    zIndex: 2,
+    position: "absolute",
+    right: 0,
+    width: 100,
+    height: 30,
+    borderRadius: 10,
+    backgroundColor: color.Emerald,
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.95,
   },
 });
