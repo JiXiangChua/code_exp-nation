ae23c21074c8c27c60a74ccd40fa1a42fe0063cd
JiXiangChua <jixiangchua2697@gmail.com>
2022-06-13 22:49:11 +0800

    Added filter capability to events screen

--- screens/event/Events.js
+++ screens/event/Events.js
@@ -1,6 +1,7 @@
-import { useState, useContext } from "react";
-import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { useState, useContext, useEffect } from "react";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import globalStyle from "../../constants/globalStyle";
+import { LoginContext } from "../../store/context/login-context";
 import NavigationHeader from "../../components/NavigationHeader";
 import FilterTab from "../../components/FilterTab";
 import EventsCard from "./components/EventsCard";
@@ -15,6 +16,35 @@ const filterTabArray = [
 ];
 
 const Events = ({ navigation }) => {
+  const { userProfile } = useContext(LoginContext);
+  const { events } = userProfile;
+
+  const [filteredEvents, setFilteredEvents] = useState(events);
+  const [filterStatus, setFilterStatus] = useState(false); //false - show on-going, true - show completed
+
+  const setFilterStatusHandler = (status) => {
+    setFilterStatus(status);
+  };
+
+  const renderEventsOnCompletedStatus = () => {
+    if (filterStatus) {
+      let filtered = events.filter((event) => {
+        return event.status === true;
+      });
+      setFilteredEvents(filtered);
+    } else {
+      setFilteredEvents(events);
+    }
+  };
+
+  useEffect(() => {
+    renderEventsOnCompletedStatus();
+  }, [filterStatus]);
+
+  const renderItem = ({ item }) => {
+    return <EventsCard item={item} onPress={() => {}} />;
+  };
+
   return (
     <SafeAreaView
       style={[globalStyle.androidNavigationTitle, styles.container]}
@@ -25,8 +55,15 @@ const Events = ({ navigation }) => {
         iconSize={30}
         onPress={() => navigation.navigate("Rewards")}
       />
-      <FilterTab array={filterTabArray} onChangeStatus={(state) => {}} />
-      <EventsCard />
+      <FilterTab
+        array={filterTabArray}
+        onChangeStatus={setFilterStatusHandler}
+      />
+      <FlatList
+        data={filteredEvents}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 };
--- screens/event/components/EventsCard.js
+++ screens/event/components/EventsCard.js
@@ -1,53 +1,25 @@
-import { useState, useContext } from "react";
-import {
-  SafeAreaView,
-  View,
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import { LoginContext } from "../../../store/context/login-context";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import color from "../../../constants/color";
 import globalStyle from "../../../constants/globalStyle";
 
-const Item = ({ item, onPress }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, styles.dropShadow]}>
-    <Image style={{ width: "100%" }} source={item.imageBackground} />
-    <View style={styles.cardtextContainer}>
-      <Text style={[globalStyle.header2]}>{item.title}</Text>
-      <Text style={[globalStyle.body1]}>{item.content}</Text>
-    </View>
-  </TouchableOpacity>
-);
-
-const EventsCard = () => {
-  const { userProfile } = useContext(LoginContext);
-  const { events } = userProfile;
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    return <Item item={item} onPress={() => setSelectedId(item.id)} />;
-  };
-
+const EventsCard = ({ item, onPress }) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={events}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.item, globalStyle.dropShadow]}
+    >
+      <Image style={{ width: "100%" }} source={item.imageBackground} />
+      <View style={styles.cardtextContainer}>
+        <Text style={[globalStyle.header2]}>{item.title}</Text>
+        <Text style={[globalStyle.body1]}>{item.content}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default EventsCard;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   cardtextContainer: {
     marginLeft: 20,
     color: color.Black,
@@ -60,11 +32,5 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: color.Black,
     borderWidth: 1,
-  },
-  dropShadow: {
-    shadowColor: "black",
-    shadowOpacity: 0.2,
-    shadowOffset: { width: 4, height: 4 },
-    shadowRadius: 3,
   },
 });
